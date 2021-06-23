086870975bede1236845caa2334b0e917438175e
Hess Wang <hess90wang@gmail.com>
2021-06-23 22:25:27 +0100

    Build Habit Component

--- .history/src/index_20200927123423.js
+++ .history/src/index_20200927123423.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './app.jsx';
-
+import '@fortawesome/fontawesome-free/js/all.js';
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
