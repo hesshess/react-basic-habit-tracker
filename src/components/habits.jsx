8b0396b88ec1dbb5b301f02a0522e8c405bd2b49
Hess Wang <hess90wang@gmail.com>
2021-06-26 15:10:49 +0100

    Build Add and Reset with REACT Refs

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
   render() {
     return (
       <div className="habits">
+        <HabitAddForm onAdd={this.props.onAdd} />
         <ul>
           {this.props.habits.map(habit => (
             <Habit
@@ -16,6 +18,9 @@ class Habits extends Component {
             />
           ))}
         </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
       </div>
     );
   }
