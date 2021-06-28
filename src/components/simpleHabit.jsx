f0660b985a2e4cf0a46c549a65856cbb326f27ba
Hess Wang <hess90wang@gmail.com>
2021-06-28 13:50:26 +0100

    REACT Hook Practice Answer

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState, useRef, useCallback, useEffect } from 'react';
 
-class SimpleHabit extends Component {
-  state = {
-    count: 0,
-  };
+const SimpleHabit = () => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
 
-  render() {
-    return (
-      <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
-          <i className="fas fa-plus-square"></i>
-        </button>
-      </li>
-    );
-  }
-}
+  useEffect(() => {
+    console.log(`mounted & updated!: ${count}`);
+  }, []);
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">
+        Reading
+      </span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
 
 export default SimpleHabit;
